94101d8542dc3304ffbeee1cd18e6962c7b37d02
lavanchuan <chuan661999@gmail.com>
2024-02-12 15:47:44 +0700

    init

---
+++ src/main/java/com/chuan/deployapi03/DeployApi03Application.java
@@ -0,0 +1,13 @@
+package com.chuan.deployapi03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployApi03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployApi03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/chuan/deployapi03/DeployApi03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuan.deployapi03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployApi03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
