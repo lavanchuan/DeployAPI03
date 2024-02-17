b1536a2e4d38b83d4d43b13a4e28dd5802a89c67
lavanchuan <chuan661999@gmail.com>
2024-02-17 11:38:12 +0700

    update home api

--- src/main/java/com/chuan/deployapi03/controllers/HelloController.java
+++ src/main/java/com/chuan/deployapi03/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.chuan.deployapi03.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
         return "hello";
+    }
+
+    @GetMapping("/")
+    public String homePage(){
+        return "Hello my friend";
     }
 }
